b0f443bb33c9da2d584c8ad8fd7b5135ce49261c
LouisOnMac <gregoryshentj@gmail.com>
2020-02-10 18:31:23 +0800

    完成5-1 http认证

--- 005 模块/requests/ch5/authen.py
+++ 005 模块/requests/ch5/authen.py
@@ -1,22 +1,48 @@
-# -*- coding: utf-8 -*-
-
-import json
 import requests
 
 BASE_URL = 'https://api.github.com'
 
 
-def construct_url(endpoint):
-    return '/'.join([BASE_URL, endpoint])
+def construct_url(end_ponit):
+    return '/'.join([BASE_URL, end_ponit])
 
-def better_print(json_str):
-    return json.dumps(json.loads(json_str), indent=4)
 
 def basic_auth():
     """基本认证
     """
-    response = requests.get(construct_url('user'), auth=('imoocdemo', 'imoocdemo123'))
-    print better_print(response.text)
-    print response.request.headers
+    response = requests.get(construct_url('user'), auth=('gregoryshentj@gmail.com', 'shenxin22019891'))
+    print(response.text)
+    print(response.request.headers)
 
-basic_auth()
+
+def basic_oauth():
+    headers = {'Authorization': 'token ff43fb7d15250964cf13d2d0ff4f8fd648ff3bb2'}
+    response = requests.get(construct_url('user/emails'))
+    print(response.request.headers)
+    print(response.text)
+    print(response.status_code)
+
+
+# basic_oauth()
+
+from requests.auth import AuthBase
+
+
+class GithubAuth(AuthBase):
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        # request 加headers
+        r.headers['Authorization'] = ' '.join(['token', self.token])
+        return r
+
+
+def oauth_advance():
+    auth = GithubAuth('ff43fb7d15250964cf13d2d0ff4f8fd648ff3bb2')
+    response = requests.get(construct_url('user/emails'), auth=auth)
+    print(response.text)
+
+
+oauth_advance()
